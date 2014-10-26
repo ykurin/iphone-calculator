aa8233c7ebe60a5e6510e491dbfaa7419b87b3db
Gryphon <yuriykurin@gmail.com>
2014-10-26 20:53:01 +0300

    First project commit. Project create.

---
+++ iphone-calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iphone-calculator
+//
+//  Created by Admin on 26.10.14.
+//  Copyright (c) 2014 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iphone-calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iphone-calculator
+//
+//  Created by Admin on 26.10.14.
+//  Copyright (c) 2014 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
